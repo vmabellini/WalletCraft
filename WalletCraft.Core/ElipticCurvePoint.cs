13e7959dd95977f69cc6a77f3b0d3d3003d0e06a
Vinicius Mabellini <vpmabellini@gmail.com>
2019-07-20 23:53:19 -0300

    Eliptic Curve and secp256k1 constraints

---
+++ WalletCraft.Core/ElipticCurvePoint.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalletCraft.Core
+{
+    public class ElipticCurvePoint : Equality<ElipticCurvePoint>
+    {
+        private readonly long _a;
+        private readonly long _b;
+        private readonly long _x;
+        private readonly long _y;
+
+        public ElipticCurvePoint(long a, long b, long x, long y)
+        {
+            _a = a;
+            _b = b;
+            _x = x;
+            _y = y;
+            //Constraint
+            //y2 = x3 + ax + b
+            if ((y * y) != (x * x * x) + (a * x) + b)
+                throw new ArgumentException("Invalid values for a eliptic curve");
+        }
+
+        public long A => _a;
+
+        public long B => _b;
+
+        public long X => _x;
+
+        public long Y => _y;
+
+        protected override bool EqualsCore(ElipticCurvePoint other)
+        {
+            return A == other.A &&
+                B == other.B &&
+                X == other.X &&
+                Y == other.Y;
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            unchecked
+            {
+                int hashCode = _a.GetHashCode();
+                hashCode = (hashCode * 397) ^ _b.GetHashCode();
+                hashCode = (hashCode * 397) ^ _x.GetHashCode();
+                hashCode = (hashCode * 397) ^ _y.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+}
---
+++ WalletCraft.Core/Secp256k1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalletCraft.Core
+{
+    public class Secp256k1 : ElipticCurvePoint
+    {
+        public Secp256k1(long x, long y) : base(0, 7, x, y)
+        {
+        }
+    }
+}
