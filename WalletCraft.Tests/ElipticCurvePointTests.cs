2ca9a42b317918ee3a8058054c86dd6a321252a8
Vinicius Mabellini <vpmabellini@gmail.com>
2019-07-21 00:02:39 -0300

    Basic tests for Eliptic Curves

---
+++ WalletCraft.Tests/ElipticCurvePointTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WalletCraft.Core;
+using Xunit;
+
+namespace WalletCraft.Tests
+{
+    public class ElipticCurvePointTests
+    {
+        [Fact]
+        public void ValidPoint()
+        {
+            new ElipticCurvePoint(5, 7, -1, -1);
+            new ElipticCurvePoint(5, 7, 18, 77);
+        }
+
+        [Fact]
+        public void InvalidPoint()
+        {
+            Assert.Throws<ArgumentException>(() => new ElipticCurvePoint(5, 7, 2, 4));
+            Assert.Throws<ArgumentException>(() => new ElipticCurvePoint(5, 7, 5, 7));
+        }
+    }
+}
